86637d1b568842bc2e5eaf650d5f538779a4c58c
rmod01 <rohitmcoder@gmail.com>
2021-03-25 05:23:39 +0530

    feat: Basic server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Test1...");
+const express = require("express");
+const app = express();
+const port = 9999;
+
+const rangeInfo = {
+    lower: 1,
+    upper: 100
+}
+
+const divisorInfo = {
+    outputDetails: [
+    {
+        divisor: 3,
+        output: "Boss"
+    },
+    {
+        divisor: 5,
+        output: "Hogg"
+    },
+    {
+        divisor: 9,
+        output: "Foggie"
+    },
+  ]
+};
+
+app.get("/", (req, res) => {
+let response = "<div>Test1 response</div>";
+res.set("Content-Type", "text/html")
+res.send(response)
+});
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+});
